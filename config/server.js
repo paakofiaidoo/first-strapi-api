2f3ad87bc38fc6c26c78a8ae22a589ca67c02118
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-11-22 23:23:38 +0000

    set up intial api

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '1ef86daec5fe146b076fb14f811c0fe0'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '615e3af7-233b-498a-b34e-153dca1c03b0'
+};
