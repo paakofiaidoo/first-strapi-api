9b62250dcfe25d7777bbb92d89b2b30dd880bf57
Paa-Kofi Aidoo <paakofiaidoo17@gmail.com>
2021-11-25 21:49:17 +0000

    setting up postgres connection

--- config/database.js
+++ config/database.js
@@ -4,8 +4,14 @@ module.exports = ({ env }) => ({
     default: {
       connector: 'bookshelf',
       settings: {
-        client: 'sqlite',
-        filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        // client: 'sqlite',
+        // filename: env('DATABASE_FILENAME', '.tmp/data.db'),
+        client: 'postgres',
+        host: env('DATABASE_HOST', process.env.POSTGRES_HOST),
+        port: env.int('DATABASE_PORT', 5432),
+        database: env('DATABASE_NAME', process.env.POSTGRES_DB),
+        username: env('DATABASE_USERNAME', process.env.POSTGRES_USER),
+        password: env('DATABASE_PASSWORD', `${process.env.POSTGRES_PASSWORD}`)
       },
       options: {
         useNullAsDefault: true,
@@ -13,3 +19,5 @@ module.exports = ({ env }) => ({
     },
   },
 });
+
+console.log(`password:${process.env.POSTGRES_HOST}`, process.env.POSTGRES_DB, process.env.POSTGRES_USER, process.env.POSTGRES_PASSWORD);
